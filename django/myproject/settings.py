eed5f60fb9c18bf0f3633bca87977cc8c0eebbf6
Matthew Cengia <mattcen@mattcen.com>
2023-06-05 11:33:28 -0700

    Update template to work with Wagtail 5.0
    This is why we should pin to specific package versions!
    
    As per https://docs.wagtail.org/en/latest/releases/5.0.html#removal-of-deprecated-features:
    > The modules `wagtail.core`, [...] are removed.

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -78,7 +78,7 @@ INSTALLED_APPS = [
     "wagtail.images",
     "wagtail.search",
     "wagtail.admin",
-    "wagtail.core",
+    "wagtail",
     "modelcluster",
     "taggit",
     # End of Wagtail includes
